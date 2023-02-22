337305db5b847a4bd2faf748a4d34903e73d777b
James Li <34687158+j4mesli@users.noreply.github.com>
2023-02-21 21:32:52 -0500

    updated wip tybwardle v2
    init new components:
      - header
      - home
    linked noclass style sheet sakura.css

---
+++ frontend/src/app/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  styleUrls: ['./header.component.css'],
+  template: `
+    <p>header works!</p>
+  `,
+})
+export class HeaderComponent {
+
+}
